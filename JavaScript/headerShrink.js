c1db4964925e31f95b2c14b2165080be39bd7c23
Jerry <random4423.work@gmail.com>
2018-10-28 21:33:05 -0500

    minor push

---
+++ JavaScript/headerShrink.js
@@ -0,0 +1,12 @@
+	$(document).on("scroll", function(){
+		if
+      ($(document).scrollTop() > 100){
+		  $("header").addClass("shrink");
+		}
+		else
+		{
+			$("header").removeClass("shrink");
+		}
+	});
+
+// https://codepen.io/tomdurkin/pen/nvAjd
